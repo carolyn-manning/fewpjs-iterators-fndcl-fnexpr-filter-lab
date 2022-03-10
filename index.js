7fd731dfcbe48ac3b4e4f6be859fc8f3dee7d9e8
carolyn-manning <carolyn.manning3@gmail.com>
2022-03-10 00:21:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 function findMatching(drivers, string) {
     return drivers.filter(driver => {
-        return driver.toLowerCase() === string;
+        return driver.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase().startsWith(string.toLowerCase());
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.name === string;
+    })
 }
